4e955b6c5732f31a41d472f8c287ccebfedac542
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-31 21:16:17 +0100

    ggf

--- src/app/pais/pages/por-regiao/por-regiao.component.ts
+++ src/app/pais/pages/por-regiao/por-regiao.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-regiao',
   templateUrl: './por-regiao.component.html',
   styles: [
+   ` button {
+      margin-right: 5px;
+    }`
   ]
 })
-export class PorRegiaoComponent implements OnInit {
+export class PorRegiaoComponent  {
+  
+
+  regioes     : string[] = ['africa', 'america', 'asia', 'auropa', 'oceania'];
+  regiaoActiva: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  activarRegiao( regiao: string) {
+    this.regiaoActiva = regiao;
+
+    
   }
 
 }
